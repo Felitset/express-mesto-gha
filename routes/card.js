796c4bca415c201fc894dd0fc6126e21dd06492f
olga <memento1991@rambler.ru>
2023-01-11 16:27:01 +0800

    joi validation avatar url

--- routes/card.js
+++ routes/card.js
@@ -12,7 +12,7 @@ router.get('/', getAllCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().uri().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+    link: Joi.string().uri().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/).required(),
   }),
 }), postCard);
 router.delete('/:cardId', celebrate({
